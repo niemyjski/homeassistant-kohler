24c02192b5ca14c79cab55bb0c1b83949779f9f3
dcmeglio <21957250+dcmeglio@users.noreply.github.com>
2023-08-31 22:45:46 -0400

    Fix 2023.9 Incompatibility (#27)

--- custom_components/kohler/water_heater.py
+++ custom_components/kohler/water_heater.py
@@ -48,7 +48,7 @@ class KohlerWaterHeater(WaterHeaterEntity):
             identifiers={(DOMAIN, self._data.macAddress())},
             manufacturer=MANUFACTURER,
             configuration_url="http://" + data.getConf(CONF_HOST),
-            default_name=DEFAULT_NAME,
+            name=DEFAULT_NAME,
             model=MODEL,
             hw_version=self._data.firmwareVersion(),
             sw_version=self._data.firmwareVersion(),
